3e70db897b2e9ead89bdbbaa02d4d526acce62a7
marialarionova <33165612+marialarionova@users.noreply.github.com>
2019-05-29 03:08:40 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -7,7 +7,7 @@ from scipy import signal
 from scipy.io import wavfile
 
 
-def add_note(sample_rate, audio, freq=None, duration_milliseconds=250, volume=0.5):
+def plus_note(sample_rate, audio, freq=None, duration_milliseconds=250, volume=0.5):
     number_of_samples = duration_milliseconds * (sample_rate / 1000.0)
     if freq:
         for x in range(int(number_of_samples)):
@@ -16,10 +16,9 @@ def add_note(sample_rate, audio, freq=None, duration_milliseconds=250, volume=0.
         audio.append(0.0)
 
 
-def write_to_file(filename, audio, sample_rate):
+def music_file(filename, audio, sample_rate):
     wav_file = wave.open(filename, 'w')
 
-    # wav parameters
     channels = 1
     sample_width = 2
     frames = len(audio)
@@ -34,23 +33,23 @@ def write_to_file(filename, audio, sample_rate):
     wav_file.close()
 
 
-def add_sample(source_audio_fname, sample_fname):
+def plus_sample(source_audio_fname, sample_fname):
     source_audio = AudioSegment.from_wav(source_audio_fname)
     sample = AudioSegment.from_wav(sample_fname)
 
-    new_audio = source_audio.overlay(sample)
-    new_audio.export('new_output.wav', format='wav')
+    new_melody = source_audio.overlay(sample)
+    new_melody.export('new_melody.wav', format='wav')
 
-    return new_audio
+    return new_melody
 
 
 def fade_audio(audio):
     source_audio = AudioSegment.from_wav(audio)
-    new_audio = source_audio.fade_in(3000).fade_out(3000)
-    new_audio.export('new_output.wav', format='wav')
+    new_melody = source_audio.fade_in(3000).fade_out(3000)
+    new_melody.export('new_melody.wav', format='wav')
 
 
-def create_spectrogram(audio):
+def create_spect(audio):
 
     sample_rate, samples = wavfile.read(audio)
     frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
@@ -63,118 +62,114 @@ def create_spectrogram(audio):
 def main():
     sample_rate = 44100.
 
-    # change it, if u need
     notes_in_freq = {'C5': 523.25, 'D5': 587.33, 'E5': 659.26, 'F5': 698.46, 'G5': 783.99, 'A5': 880., 'H5': 987.77,
                      'C6': 1046.5, 'C7': 1046.5, 'D6': 1174.7,
                      'C4': 261.63, 'D4': 293.66, 'E4': 329.63, 'F4': 349.23, 'G4': 392.00, 'A4': 440., 'H4': 493.88}
 
-    # create simple melody for example
     audio = []
 
-    add_note(sample_rate, audio, notes_in_freq['G4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['G4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['G4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['A4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['G4'], 500)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['E4'], 500)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['F4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['G4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['F4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['E4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['D4'], 750)
-    add_note(sample_rate, audio, duration_milliseconds=250)
-    add_note(sample_rate, audio, notes_in_freq['G4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['G4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['G4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['A4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['G4'], 500)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['E4'], 500)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['G4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['C5'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['H4'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['D5'], 250)
-    add_note(sample_rate, audio, duration_milliseconds=100)
-    add_note(sample_rate, audio, notes_in_freq['C5'], 750)
-    add_note(sample_rate, audio, duration_milliseconds=250)
+    plus_note(sample_rate, audio, notes_in_freq['G4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['G4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['G4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['A4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['G4'], 500)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['E4'], 500)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['F4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['G4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['F4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['E4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['D4'], 750)
+    plus_note(sample_rate, audio, duration_milliseconds=250)
+    plus_note(sample_rate, audio, notes_in_freq['G4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['G4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['G4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['A4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['G4'], 500)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['E4'], 500)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['G4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['C5'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['H4'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['D5'], 250)
+    plus_note(sample_rate, audio, duration_milliseconds=100)
+    plus_note(sample_rate, audio, notes_in_freq['C5'], 750)
+    plus_note(sample_rate, audio, duration_milliseconds=250)
 
-    write_to_file('output.wav', audio, sample_rate)
+    music_to_file('melody.wav', audio, sample_rate)
 
-    # create a sample with an octave interval from source audio
     sample = []
 
+    plus_note(sample_rate, sample, notes_in_freq['G5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['G5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['G5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['A5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['G5'], 500)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['E5'], 500)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['F5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['G5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['F5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['E5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['D5'], 750)
+    plus_note(sample_rate, sample, duration_milliseconds=250)
+    plus_note(sample_rate, sample, notes_in_freq['G5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['G5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['G5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['A5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['G5'], 500)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['E5'], 500)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['G5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['C6'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['H5'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['D6'], 250)
+    plus_note(sample_rate, sample, duration_milliseconds=100)
+    plus_note(sample_rate, sample, notes_in_freq['C6'], 750)
+    plus_note(sample_rate, sample, duration_milliseconds=250)
 
-    add_note(sample_rate, sample, notes_in_freq['G5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['G5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['G5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['A5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['G5'], 500)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['E5'], 500)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['F5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['G5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['F5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['E5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['D5'], 750)
-    add_note(sample_rate, sample, duration_milliseconds=250)
-    add_note(sample_rate, sample, notes_in_freq['G5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['G5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['G5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['A5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['G5'], 500)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['E5'], 500)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['G5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['C6'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['H5'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['D6'], 250)
-    add_note(sample_rate, sample, duration_milliseconds=100)
-    add_note(sample_rate, sample, notes_in_freq['C6'], 750)
-    add_note(sample_rate, sample, duration_milliseconds=250)
+    music_to_file('sample.wav', sample, sample_rate)
 
-    write_to_file('sample.wav', sample, sample_rate)
-
-    audio, sample = 'output.wav', 'sample.wav'
-    add_sample(audio, sample)
-    audio = 'new_output.wav'
+    audio, sample = 'melody.wav', 'sample.wav'
+    plus_sample(audio, sample)
+    audio = 'new_melody.wav'
     fade_audio(audio)
 
-    create_spectrogram(audio)
+    create_spect(audio)
 
 
 if __name__ == "__main__":
